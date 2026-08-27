6bf1bb0af022d9f1d6b4a8a5973e6274d3ac967b
Odwa <odwamtatambi@gmail.com>
2026-08-27 03:02:39 +0200

    Add: Date Extension on utils package

--- app/src/main/java/com/odwa/moodtracker/util/DateExtensions.kt
+++ app/src/main/java/com/odwa/moodtracker/util/DateExtensions.kt
@@ -1,13 +1,18 @@
 package com.odwa.moodtracker.util
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
+@RequiresApi(Build.VERSION_CODES.O)
+private val dateTimeFormatter = DateTimeFormatter.ofPattern("EE,dd MMM • HH:mm")
+
+@RequiresApi(Build.VERSION_CODES.O)
 fun Long.toReadableDateTime(): String {
-    val formatter = DateTimeFormatter.ofPattern("EE,dd MMM • HH:mm")
     return Instant.ofEpochMilli(this)
         .atZone(ZoneId.systemDefault())
         .toLocalDateTime()
-        .format(formatter)
+        .format(dateTimeFormatter)
 }
